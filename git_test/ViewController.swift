7ebeb419eff8ccbf710c5d01d29aa385979ea255
Macbook Air <Macbookair@Macbooks-MacBook-Air.local>
2015-09-13 17:32:16 +0700

    Initial Commit

---
+++ git_test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  git_test
+//
+//  Created by Macbook Air on 9/13/2558 BE.
+//  Copyright (c) 2558 Code-Aholic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
